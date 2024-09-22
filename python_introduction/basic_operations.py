e9cdcbac2acbee2305f4fa2bd8a6e4676648bf0e
Deann07 <a.elshimy.cf@gmail.com>
2024-09-22 23:21:06 +0300

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+number3 = number1+number2
+number4 = number1-number2
+number5 = number1*number2
+print(f"addition of {number1}and{number2} is {number3} ")
+print(f"subtraction of {number1}and{number2} is {number4} ")
+print(f"multiplication of {number1}and{number2} is {number5} ")
